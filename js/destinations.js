ae705c08a74bc6d3cafe9e44210e22c973dbe3f8
James Hooper <jameshooperdesigns@gmail.com>
2019-02-19 14:52:04 -0700

    Correct the proper syntax of the JSON script for the description

--- js/destinations.js
+++ js/destinations.js
@@ -2,29 +2,18 @@
 {
   "@context": "http://schema.org",
   "@type": "Cruise Line",
+  "name": "Cabot Cruises", 
+  "description": "Cabot Cruises is a travel agency providing superior service with amazing deals on our online travel website.",
+  "openingHours": "Mo-Fr 09:00-16:30",
+  "telephone": "+18665771000",
+  "faxNumber": "+18665775555",
   "address": {
     "@type": "PostalAddress",
+    "streetAddress": "500 South Collins Avenue Suite 100",
     "addressLocality": "Miami",
     "addressRegion": "FL",
-    "postalCode": "33410",
-    "streetAddress": "500 South Collins Avenue Suite 100"
+    "postalCode": "33410"
   },
-  "name": "Cabot Cruises",
-  "openingHours": [
-    "Mo-Fr 09:00-16:30"
-  ],
-  "priceRange": "$$",
-  "servesCarribean": [
-    "Aruba", 
-    "Bahamas",
-    "Barbados",
-    "Cayman Islands",
-    "Domincan Republic",
-    "Jamaica",
-    "Martinique",
-    "St Lucia",
-    "Trinidad and Tobago"
-  ],
-  "telephone": "(866) 577-1000",
-  "url": "http://www.cabotcruises.com"
+  "image": "http://uxpays.com/DGM3740-HTML5-Template/img/CC-Logo-MD.svg",
+  "priceRange": "$500 to $10000"
 }
